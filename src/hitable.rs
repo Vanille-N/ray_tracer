385ee085b038d4975f83ccd7473c579067287309
Vanille-N <neven.villani@gmail.com>
2020-04-29 19:54:11 +0200

    Schlick correction

--- src/hitable.rs
+++ src/hitable.rs
@@ -4,39 +4,6 @@ use crate::rgb::RGB;
 use crate::vec3::Vec3;
 use crate::EPSILON;
 
-// Warning:
-// To get a working multithreading, and because I was tired of cryptic errors:
-
-// error[E0277]:`std::ptr::NonNull<rand::rngs::adapter::ReseedingRng<
-// rand_chacha::chacha::ChaCha20Core, rand::rngs::OsRng>>` cannot be
-// sent between threads safely
-//    --> src/main.rs:142:28
-//     |
-// 142 |     writers.par_iter_mut().for_each(|(f, range)| {
-//     |                            ^^^^^^^^ `std::ptr::NonNull<rand::rngs::ada
-// pter::ReseedingRng<rand_chacha::chacha::ChaCha20Core, rand::rngs::OsRng>>` cannot
-// be sent between threads safely
-//     |
-//     = help: within `rand::prelude::ThreadRng`, the trait `std::marker::Send`
-// is not implemented for `std::ptr::NonNull<rand::rngs::adapter::Reseedin
-// gRng<rand_chacha::chacha::ChaCha20Core, rand::rngs::OsRng>>`
-//    = note: required because it appears within the type `rand::prelude::ThreadRng`
-//    = note: required because of the requirements on the impl of `std::marker::
-// Send` for `&mut rand::prelude::ThreadRng`
-//    = note: required because it appears within the type `[closure@src
-// /main.rs:142:37: 163:6 ni:&i32, nj:&i32, ns:&i32, rng:&mut
-//  rand::prelude::ThreadRng, cam:&camera::Camera, w:&hitable::World<'_>]`
-
-// I settled for some horrible design decisions.
-// Maybe this will improve in the future, when I become more comfortable with traits.
-// The good news is that this seems to have negligible effect on performance,
-// and it is barely visible from the outside of this file.
-// From the point of view of someone creating objects, it does require implementing
-// the `build` method on all objects. Since complex objects already require it, this
-// can be considered a minimal hassle.
-// Altogether it seems to be a small price to pay for access to multithreading and
-// relief from manually managing lifetimes.
-
 #[derive(Clone, Copy)]
 pub enum Primitive {
     Sphere(Sphere),
@@ -320,8 +287,8 @@ pub enum Texture {
 }
 
 //  https://en.wikipedia.org/wiki/Schlick's_approximation
-fn schlick(cos: f64, idx: f64) -> f64 {
-    let r = ((1.0 - idx) / (1.0 + idx)).powi(2);
+fn schlick(cos: f64, n1: f64, n2: f64) -> f64 {
+    let r = ((n1 - n2) / (n1 + n2)).powi(2);
     r + (1.0 - r) * (1.0 - cos).powi(5)
 }
 
@@ -410,7 +377,7 @@ pub fn scatter(incident: &Ray, record: ActiveHit, w: &World) -> Option<(RGB, Ray
                     },
                 )),
                 Some(refracted) => {
-                    let prob_reflect = schlick(cos, rel_idx);
+                    let prob_reflect = schlick(cos, i_idx, r_idx);
                     if rand::random::<f64>() < prob_reflect {
                         Some((
                             shade,
