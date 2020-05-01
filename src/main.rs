6621cd1a0cbaee8de9f1bbaef905528800d85a17
Vanille-N <neven.villani@gmail.com>
2020-05-01 13:53:19 +0200

    Wrappers

--- src/main.rs
+++ src/main.rs
@@ -6,6 +6,7 @@ extern crate rayon;
 use rayon::prelude::*;
 use std::fs::File;
 use std::io::{BufWriter, Write};
+use std::ops::Range;
 use std::process::Command;
 
 mod camera;
@@ -33,62 +34,69 @@ use vec3::Vec3;
 
 const EPSILON: f64 = 0.000_000_1;
 
-const CFG: &str = "linux";
+const SYS: &str = "linux";
 
 #[allow(unused_variables)]
 fn main() {
-    let (ni, nj, ns, cam, w, sky) = build_world();
+    let cfg = build_world();
     let nb_cores = 5;
     let mut writers = Vec::new();
-    for idx in (0..nb_cores).rev() {
+    for id in 0..nb_cores {
         //let out = File::create(&format!(".out{}.txt", idx)).unwrap();
-        let out = BufWriter::new(File::create(&format!(".out{}.txt", idx)).unwrap());
-        let ni_rng = (idx * ni / nb_cores)..((idx + 1) * ni / nb_cores);
-        writers.push((idx, out, ni_rng, cam.clone(), w.clone(), sky.clone()));
+        let out = BufWriter::new(File::create(&format!(".out{}.txt", id)).unwrap());
+        let rng = (id * cfg.hgt / nb_cores)..((id + 1) * cfg.hgt / nb_cores);
+        writers.push(Writer {
+            id: id as u8,
+            stdout: out,
+            rng,
+            cam: cfg.cam.clone(),
+            world: cfg.world.clone(),
+            sky: cfg.sky.clone(),
+        });
     }
 
-    if CFG == "linux" {
+    if SYS == "linux" {
         eprint!("\n\nRendering image...\n");
         eprint!("|\x1b[50C|\x1b[1A\n");
     }
-    writers.par_iter_mut().for_each(|(id, f, range, cam, w, sky)| {
-        let color = &format!("\x1b[3{}m", *id + 1);
-        for i in range.rev() {
-            if CFG == "linux" {
-                if i * 100 % ni == 0 {
-                    let load = 100 - i * 100 / ni;
+    writers.par_iter_mut().for_each(|w| {
+        let color = &format!("\x1b[3{}m", w.id + 1);
+        let ni = cfg.hgt as f64;
+        let nj = cfg.wth as f64;
+        for i in w.rng.clone().rev() {
+            if SYS == "linux" {
+                if i * 100 % cfg.hgt == 0 {
+                    let load = 100 - i * 100 / cfg.hgt;
                     if load % 2 == 0 {
                         eprint!("\x1b[2B\x1b[{}C{}█\x1b[3A\n", load / 2, color);
                     }
                 }
-            } else if i * 100 % ni == 0 {
-                let load = 100 - i * 100 / ni;
+            } else if i * 100 % cfg.hgt == 0 {
+                let load = 100 - i * 100 / cfg.hgt;
                 eprintln!("{}%", load);
             }
 
-            for j in 0..nj {
-                let mut c = RGB::new(0., 0., 0.);
+            for j in 0..cfg.wth {
+                let mut c = BLACK;
                 let i = i as f64;
                 let j = j as f64;
-                let ni = ni as f64;
-                let nj = nj as f64;
-                for _ in 0..ns {
+                for _ in 0..cfg.iter {
                     let v = (i + rand::random::<f64>()) / ni;
                     let u = (j + rand::random::<f64>()) / nj;
-                    let r = cam.get_ray(u, v);
-                    c += hitable::color(&r, &w, 0, sky);
+                    let r = cfg.cam.get_ray(u, v);
+                    c += hitable::color(&r, &w.world, 0, &w.sky);
                 }
-                write!(f, "{}", c / ns as f64).unwrap();
-                //f.write(format!("{}", c / ns as f64).as_bytes()).unwrap();
+                write!(w.stdout, "{}", c / cfg.iter as f64).unwrap();
             }
-            writeln!(f).unwrap();
-            //f.write(b"\n").unwrap();
+            writeln!(w.stdout).unwrap();
         }
-        f.flush().unwrap();
+        w.stdout.flush().unwrap();
     });
-    print!("\n\n\n\x1b[0m");
+    if SYS == "linux" {
+        eprint!("\n\n\n\x1b[0m");
+    }
     let mut f = File::create("img.ppm").unwrap();
-    writeln!(f, "P3\n{} {}\n255", nj, ni).unwrap();
+    writeln!(f, "P3\n{} {}\n255", cfg.wth, cfg.hgt).unwrap();
     for idx in (0..nb_cores).rev() {
         let output = Command::new("cat")
             .arg(&format!(".out{}.txt", idx))
@@ -98,10 +106,29 @@ fn main() {
     }
 }
 
-fn build_world() -> (i32, i32, i32, Camera, World, Sky) {
-    let nj = 1000; // width in pixels
-    let ni = 1000; // height in pixels
-    let ns = 50; // number of samples per pixel
+struct Cfg {
+    hgt: usize,
+    wth: usize,
+    iter: usize,
+    cam: Camera,
+    world: World,
+    sky: Sky,
+}
+
+#[derive(Clone)]
+struct Writer<W: Write> {
+    id: u8,
+    stdout: W,
+    rng: Range<usize>,
+    cam: Camera,
+    world: World,
+    sky: Sky,
+}
+
+fn build_world() -> Cfg {
+    let wth = 200; // width in pixels
+    let hgt = 100; // height in pixels
+    let iter = 50; // number of samples per pixel
     let cam = Camera::new_relative(
         Vec3::new(0.0, 1.0, 0.0), // target
         180.0,                     // angle (degrees)
@@ -109,10 +136,10 @@ fn build_world() -> (i32, i32, i32, Camera, World, Sky) {
         7.0,                      // distance (meters),
         0.0,                      // tilt (degrees)
         40.0,                     // aperture (degrees)
-        nj as f64 / ni as f64,    // aspect ratio
+        wth as f64 / hgt as f64,    // aspect ratio
     );
     let sky = Sky::new("data/sky.ppm");
-    let mut w = World::new();
+    let mut world = World::new();
     let ground = InfinitePlane {
         orig: Vec3::new(0.0, 0.0, 0.0),
         normal: Vec3::new(0.0, 1.0, 0.0),
@@ -130,9 +157,9 @@ fn build_world() -> (i32, i32, i32, Camera, World, Sky) {
     .build()
     .wrap();
 
-    w.push(ball);
+    world.push(ball);
 
-    w.push(ground);
+    world.push(ground);
 
-    (ni, nj, ns, cam, w, sky)
+    Cfg { hgt, wth, iter, cam, world, sky }
 }
