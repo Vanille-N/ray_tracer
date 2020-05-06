fc9cc1442d37598ca55d356dfe37dccced246941
Vanille-N <neven.villani@gmail.com>
2020-05-06 15:31:07 +0200

    Refactor

--- src/composite_cradle.rs
+++ src/composite_cradle.rs
@@ -27,226 +27,93 @@ impl NewtonCradle {
             w: w * 0.1,
             texture: plastic,
         }
-        .build();
-        let pillar1 = EmptyCylinder {
-            center1: self.a + v * 0.1 + u * 0.1,
-            center2: self.a + v * 0.1 + u * 0.1 + w,
-            radius: v.len() * 0.03,
-            texture: plastic,
-        }
-        .build();
-        let pillar2 = EmptyCylinder {
-            center1: self.a + v * 0.9 + u * 0.1,
-            center2: self.a + v * 0.9 + u * 0.1 + w,
-            radius: v.len() * 0.03,
-            texture: plastic,
-        }
-        .build();
-        let pillar3 = EmptyCylinder {
-            center1: self.a + v * 0.9 + u * 0.9,
-            center2: self.a + v * 0.9 + u * 0.9 + w,
-            radius: v.len() * 0.03,
-            texture: plastic,
-        }
-        .build();
-        let pillar4 = EmptyCylinder {
-            center1: self.a + v * 0.1 + u * 0.9,
-            center2: self.a + v * 0.1 + u * 0.9 + w,
-            radius: v.len() * 0.03,
-            texture: plastic,
-        }
-        .build();
-        let bar1 = EmptyCylinder {
-            center1: self.a + v * 0.1 + u * 0.1 + w,
-            center2: self.a + v * 0.9 + u * 0.1 + w,
-            radius: v.len() * 0.03,
-            texture: plastic,
-        }
-        .build();
-        let bar2 = EmptyCylinder {
-            center1: self.a + v * 0.1 + u * 0.9 + w,
-            center2: self.a + v * 0.9 + u * 0.9 + w,
-            radius: v.len() * 0.03,
-            texture: plastic,
-        }
-        .build();
-        let smoothtop1 = Sphere {
-            center: self.a + v * 0.1 + u * 0.1 + w,
-            radius: v.len() * 0.03,
-            texture: plastic,
-        }
-        .build();
-        let smoothtop2 = Sphere {
-            center: self.a + v * 0.1 + u * 0.9 + w,
-            radius: v.len() * 0.03,
-            texture: plastic,
-        }
-        .build();
-        let smoothtop3 = Sphere {
-            center: self.a + v * 0.9 + u * 0.9 + w,
-            radius: v.len() * 0.03,
-            texture: plastic,
-        }
-        .build();
-        let smoothtop4 = Sphere {
-            center: self.a + v * 0.9 + u * 0.1 + w,
-            radius: v.len() * 0.03,
-            texture: plastic,
-        }
-        .build();
+        .build()
+        .wrap();
+        let make_pillar = |c1, c2| {
+            EmptyCylinder {
+                center1: c1,
+                center2: c2,
+                radius: v.len() * 0.03,
+                texture: plastic,
+            }
+            .build()
+            .wrap()
+        };
+        let pillar1 = make_pillar(self.a + v * 0.1 + u * 0.1, self.a + v * 0.1 + u * 0.1 + w);
+        let pillar2 = make_pillar(self.a + v * 0.9 + u * 0.1, self.a + v * 0.9 + u * 0.1 + w);
+        let pillar3 = make_pillar(self.a + v * 0.9 + u * 0.9, self.a + v * 0.9 + u * 0.9 + w);
+        let pillar4 = make_pillar(self.a + v * 0.1 + u * 0.9, self.a + v * 0.1 + u * 0.9 + w);
+        let bar1 = make_pillar(
+            self.a + v * 0.1 + u * 0.1 + w,
+            self.a + v * 0.9 + u * 0.1 + w,
+        );
+        let bar2 = make_pillar(
+            self.a + v * 0.1 + u * 0.9 + w,
+            self.a + v * 0.9 + u * 0.9 + w,
+        );
+
+        let make_cap = |c| {
+            Sphere {
+                center: c,
+                radius: v.len() * 0.03,
+                texture: plastic,
+            }
+            .build()
+            .wrap()
+        };
+        let smoothtop1 = make_cap(self.a + v * 0.1 + u * 0.1 + w);
+        let smoothtop2 = make_cap(self.a + v * 0.1 + u * 0.9 + w);
+        let smoothtop3 = make_cap(self.a + v * 0.9 + u * 0.9 + w);
+        let smoothtop4 = make_cap(self.a + v * 0.9 + u * 0.1 + w);
         let r = u.len() * 0.7 / 10.0;
-        let sphere1 = Sphere {
-            center: self.a + u * 0.5 + v * 0.15 + v.unit() * r + w * 0.3,
-            radius: r,
-            texture: steel,
+
+        let make_ball = |c| {
+            let sphere = Sphere {
+                center: c,
+                radius: r,
+                texture: steel,
+            };
+            let ring = Sphere {
+                center: sphere.center + w.unit() * r,
+                radius: r * 0.3,
+                texture: plastic,
+            };
+            let threada = EmptyCylinder {
+                center1: ring.center,
+                center2: ring.center + u * 0.40 + w * 0.64,
+                radius: r * 0.03,
+                texture: nylon,
+            }
+            .build();
+            let threadb = EmptyCylinder {
+                center1: ring.center,
+                center2: ring.center - u * 0.40 + w * 0.64,
+                radius: r * 0.03,
+                texture: nylon,
+            }
+            .build();
+            (
+                sphere.build().wrap(),
+                ring.build().wrap(),
+                threada.wrap(),
+                threadb.wrap(),
+            )
         };
-        let ring1 = Sphere {
-            center: sphere1.center + w.unit() * r,
-            radius: r * 0.3,
-            texture: plastic,
-        };
-        let thread1a = EmptyCylinder {
-            center1: ring1.center,
-            center2: ring1.center + u * 0.40 + w * 0.64,
-            radius: r * 0.03,
-            texture: nylon,
-        }
-        .build();
-        let thread1b = EmptyCylinder {
-            center1: ring1.center,
-            center2: ring1.center - u * 0.40 + w * 0.64,
-            radius: r * 0.03,
-            texture: nylon,
-        }
-        .build();
-        let sphere2 = Sphere {
-            center: self.a + u * 0.5 + v * 0.15 + v.unit() * 3. * r + w * 0.3,
-            radius: r,
-            texture: steel,
-        };
-        let ring2 = Sphere {
-            center: sphere2.center + w.unit() * r,
-            radius: r * 0.3,
-            texture: plastic,
-        };
-        let thread2a = EmptyCylinder {
-            center1: ring2.center,
-            center2: ring2.center + u * 0.40 + w * 0.64,
-            radius: r * 0.03,
-            texture: nylon,
-        }
-        .build();
-        let thread2b = EmptyCylinder {
-            center1: ring2.center,
-            center2: ring2.center - u * 0.40 + w * 0.64,
-            radius: r * 0.03,
-            texture: nylon,
-        }
-        .build();
-        let sphere3 = Sphere {
-            center: self.a + u * 0.5 + v * 0.15 + v.unit() * 5. * r + w * 0.3,
-            radius: r,
-            texture: steel,
-        };
-        let ring3 = Sphere {
-            center: sphere3.center + w.unit() * r,
-            radius: r * 0.3,
-            texture: plastic,
-        };
-        let thread3a = EmptyCylinder {
-            center1: ring3.center,
-            center2: ring3.center + u * 0.40 + w * 0.64,
-            radius: r * 0.03,
-            texture: nylon,
-        }
-        .build();
-        let thread3b = EmptyCylinder {
-            center1: ring3.center,
-            center2: ring3.center - u * 0.40 + w * 0.64,
-            radius: r * 0.03,
-            texture: nylon,
-        }
-        .build();
-        let sphere4 = Sphere {
-            center: self.a + u * 0.5 + v * 0.15 + v.unit() * 7. * r + w * 0.3,
-            radius: r,
-            texture: steel,
-        };
-        let ring4 = Sphere {
-            center: sphere4.center + w.unit() * r,
-            radius: r * 0.3,
-            texture: plastic,
-        };
-        let thread4a = EmptyCylinder {
-            center1: ring4.center,
-            center2: ring4.center + u * 0.40 + w * 0.64,
-            radius: r * 0.03,
-            texture: nylon,
-        }
-        .build();
-        let thread4b = EmptyCylinder {
-            center1: ring4.center,
-            center2: ring4.center - u * 0.40 + w * 0.64,
-            radius: r * 0.03,
-            texture: nylon,
-        }
-        .build();
-        let sphere5 = Sphere {
-            center: self.a + u * 0.5 + v * 0.15 + v.unit() * 9. * r + w * 0.3,
-            radius: r,
-            texture: steel,
-        };
-        let ring5 = Sphere {
-            center: sphere5.center + w.unit() * r,
-            radius: r * 0.3,
-            texture: plastic,
-        };
-        let thread5a = EmptyCylinder {
-            center1: ring5.center,
-            center2: ring5.center + u * 0.40 + w * 0.64,
-            radius: r * 0.03,
-            texture: nylon,
-        }
-        .build();
-        let thread5b = EmptyCylinder {
-            center1: ring5.center,
-            center2: ring5.center - u * 0.40 + w * 0.64,
-            radius: r * 0.03,
-            texture: nylon,
-        }
-        .build();
+        let (sphere1, ring1, thread1a, thread1b) =
+            make_ball(self.a + u * 0.5 + v * 0.15 + v.unit() * 3. * r + w * 0.3);
+        let (sphere2, ring2, thread2a, thread2b) =
+            make_ball(self.a + u * 0.5 + v * 0.15 + v.unit() * 3. * r + w * 0.3);
+        let (sphere3, ring3, thread3a, thread3b) =
+            make_ball(self.a + u * 0.5 + v * 0.15 + v.unit() * 5. * r + w * 0.3);
+        let (sphere4, ring4, thread4a, thread4b) =
+            make_ball(self.a + u * 0.5 + v * 0.15 + v.unit() * 7. * r + w * 0.3);
+        let (sphere5, ring5, thread5a, thread5b) =
+            make_ball(self.a + u * 0.5 + v * 0.15 + v.unit() * 9. * r + w * 0.3);
         vec![
-            pedestal.wrap(),
-            pillar1.wrap(),
-            pillar2.wrap(),
-            pillar3.wrap(),
-            pillar4.wrap(),
-            bar1.wrap(),
-            bar2.wrap(),
-            smoothtop1.wrap(),
-            smoothtop2.wrap(),
-            smoothtop3.wrap(),
-            smoothtop4.wrap(),
-            sphere1.build().wrap(),
-            sphere2.build().wrap(),
-            sphere3.build().wrap(),
-            sphere4.build().wrap(),
-            sphere5.build().wrap(),
-            ring1.build().wrap(),
-            ring2.build().wrap(),
-            ring3.build().wrap(),
-            ring4.build().wrap(),
-            ring5.build().wrap(),
-            thread1a.wrap(),
-            thread1b.wrap(),
-            thread2a.wrap(),
-            thread2b.wrap(),
-            thread3a.wrap(),
-            thread3b.wrap(),
-            thread4a.wrap(),
-            thread4b.wrap(),
-            thread5a.wrap(),
-            thread5b.wrap(),
+            pedestal, pillar1, pillar2, pillar3, pillar4, bar1, bar2, smoothtop1, smoothtop2,
+            smoothtop3, smoothtop4, sphere1, sphere2, sphere3, sphere4, sphere5, ring1, ring2,
+            ring3, ring4, ring5, thread1a, thread1b, thread2a, thread2b, thread3a, thread3b,
+            thread4a, thread4b, thread5a, thread5b,
         ]
     }
 }
