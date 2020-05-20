ac0daee54d9adaeb9cd824b24c76fa14b056ce6f
Vanille-N <neven.villani@gmail.com>
2020-05-20 14:24:56 +0200

    update sample python code

--- trace.py
+++ trace.py
@@ -3,7 +3,9 @@
 import pytrace as tr
 
 trace = tr.Cfg(100, 100, 20)
-trace.make_cam()
-trace.make_sky()
+trace.add_cam(tr.Camera(25, 25, 0))
+trace.add_sky(tr.Sky.blank())
 trace.add_obj()
+
+trace.silence()
 trace.render()
