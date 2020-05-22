29af411877c30fd427f706a52d5103248eea8afc
Vanille-N <neven.villani@gmail.com>
2020-05-22 13:30:14 +0200

    File reduced with macros

--- src/external/primitives.rs
+++ src/external/primitives.rs
@@ -47,388 +47,65 @@ macro_rules! dvp {
     }
 }
 
-
-
-
-
-
-
-#[pyclass]
-#[derive(Copy, Clone)]
-pub struct Sphere {
-    #[pyo3(get, set)]
-    pub center: Vec3,
-    #[pyo3(get, set)]
-    pub radius: f64,
-    #[pyo3(get, set)]
-    pub texture: Texture,
+dvp!{Sphere,
+    center: Vec3,
+    radius: f64,
 }
 
-#[pymethods]
-impl Sphere {
-    #[new]
-    pub fn new(center: Vec3, radius: f64, texture: Texture) -> Self {
-        Self {
-            center,
-            radius,
-            texture,
-        }
-    }
+dvp!{InfinitePlane,
+    orig: Vec3,
+    normal: Vec3,
 }
 
-impl ToInternal for Sphere {
-    fn to_internal(self) -> internal::Primitive {
-        internal::Sphere {
-            center: self.center.to_internal(),
-            radius: self.radius,
-            texture: self.texture.to_internal(),
-        }
-        .build()
-    }
+dvp!{Triangle,
+    a: Vec3,
+    u: Vec3,
+    v: Vec3,
 }
 
-#[pyclass]
-#[derive(Clone, Copy)]
-pub struct InfinitePlane {
-    #[pyo3(get, set)]
-    pub orig: Vec3,
-    #[pyo3(get, set)]
-    pub normal: Vec3,
-    #[pyo3(get, set)]
-    pub texture: Texture,
+dvp!{Parallelogram,
+    a: Vec3,
+    u: Vec3,
+    v: Vec3,
 }
 
-#[pymethods]
-impl InfinitePlane {
-    #[new]
-    pub fn new(orig: Vec3, normal: Vec3, texture: Texture) -> Self {
-        Self {
-            orig,
-            normal,
-            texture,
-        }
-    }
+dvp!{Rhomboid,
+    a: Vec3,
+    u: Vec3,
+    v: Vec3,
+    w: Vec3,
 }
 
-impl ToInternal for InfinitePlane {
-    fn to_internal(self) -> internal::Primitive {
-        internal::InfinitePlane {
-            orig: self.orig.to_internal(),
-            normal: self.normal.to_internal(),
-            texture: self.texture.to_internal(),
-        }
-        .build()
-    }
+dvp!{EmptyCylinder,
+    center1: Vec3,
+    center2: Vec3,
+    radius: f64,
 }
 
-#[pyclass]
-#[derive(Clone, Copy)]
-pub struct Triangle {
-    #[pyo3(get, set)]
-    pub a: Vec3,
-    #[pyo3(get, set)]
-    pub u: Vec3,
-    #[pyo3(get, set)]
-    pub v: Vec3,
-    #[pyo3(get, set)]
-    pub texture: Texture,
+dvp!{Disc,
+    center: Vec3,
+    normal: Vec3,
+    radius: f64,
 }
 
-#[pymethods]
-impl Triangle {
-    #[new]
-    pub fn new(a: Vec3, u: Vec3, v: Vec3, texture: Texture) -> Self {
-        Self { a, u, v, texture }
-    }
+dvp!{Cylinder,
+    center1: Vec3,
+    center2: Vec3,
+    radius: f64,
 }
 
-impl ToInternal for Triangle {
-    fn to_internal(self) -> internal::Primitive {
-        internal::Triangle {
-            a: self.a.to_internal(),
-            u: self.u.to_internal(),
-            v: self.v.to_internal(),
-            texture: self.texture.to_internal(),
-        }
-        .build()
-    }
+dvp!{EmptyCone,
+    orig: Vec3,
+    dir: Vec3,
+    angle: f64,
+    begin: f64,
+    end: f64,
 }
 
-#[pyclass]
-#[derive(Clone, Copy)]
-pub struct Parallelogram {
-    #[pyo3(get, set)]
-    pub a: Vec3,
-    #[pyo3(get, set)]
-    pub u: Vec3,
-    #[pyo3(get, set)]
-    pub v: Vec3,
-    #[pyo3(get, set)]
-    pub texture: Texture,
-}
-
-#[pymethods]
-impl Parallelogram {
-    #[new]
-    pub fn new(a: Vec3, u: Vec3, v: Vec3, texture: Texture) -> Self {
-        Self { a, u, v, texture }
-    }
-}
-
-impl ToInternal for Parallelogram {
-    fn to_internal(self) -> internal::Primitive {
-        internal::Parallelogram {
-            a: self.a.to_internal(),
-            u: self.u.to_internal(),
-            v: self.v.to_internal(),
-            texture: self.texture.to_internal(),
-        }
-        .build()
-    }
-}
-
-#[pyclass]
-#[derive(Clone, Copy)]
-pub struct Rhomboid {
-    #[pyo3(get, set)]
-    pub a: Vec3,
-    #[pyo3(get, set)]
-    pub u: Vec3,
-    #[pyo3(get, set)]
-    pub v: Vec3,
-    #[pyo3(get, set)]
-    pub w: Vec3,
-    #[pyo3(get, set)]
-    pub texture: Texture,
-}
-
-#[pymethods]
-impl Rhomboid {
-    #[new]
-    pub fn new(a: Vec3, u: Vec3, v: Vec3, w: Vec3, texture: Texture) -> Self {
-        Self {
-            a,
-            u,
-            v,
-            w,
-            texture,
-        }
-    }
-}
-
-impl ToInternal for Rhomboid {
-    fn to_internal(self) -> internal::Primitive {
-        internal::Rhomboid {
-            a: self.a.to_internal(),
-            u: self.u.to_internal(),
-            v: self.v.to_internal(),
-            w: self.w.to_internal(),
-            texture: self.texture.to_internal(),
-        }
-        .build()
-    }
-}
-
-#[pyclass]
-#[derive(Clone, Copy)]
-pub struct EmptyCylinder {
-    #[pyo3(get, set)]
-    pub center1: Vec3,
-    #[pyo3(get, set)]
-    pub center2: Vec3,
-    #[pyo3(get, set)]
-    pub radius: f64,
-    #[pyo3(get, set)]
-    pub texture: Texture,
-}
-
-#[pymethods]
-impl EmptyCylinder {
-    #[new]
-    pub fn new(center1: Vec3, center2: Vec3, radius: f64, texture: Texture) -> Self {
-        Self {
-            center1,
-            center2,
-            radius,
-            texture,
-        }
-    }
-}
-
-impl ToInternal for EmptyCylinder {
-    fn to_internal(self) -> internal::Primitive {
-        internal::EmptyCylinder {
-            center1: self.center1.to_internal(),
-            center2: self.center2.to_internal(),
-            radius: self.radius,
-            texture: self.texture.to_internal(),
-        }
-        .build()
-    }
-}
-
-#[pyclass]
-#[derive(Clone, Copy)]
-pub struct Disc {
-    #[pyo3(get, set)]
-    pub center: Vec3,
-    #[pyo3(get, set)]
-    pub normal: Vec3,
-    #[pyo3(get, set)]
-    pub radius: f64,
-    #[pyo3(get, set)]
-    pub texture: Texture,
-}
-
-#[pymethods]
-impl Disc {
-    #[new]
-    pub fn new(center: Vec3, normal: Vec3, radius: f64, texture: Texture) -> Self {
-        Self {
-            center,
-            normal,
-            radius,
-            texture,
-        }
-    }
-}
-
-impl ToInternal for Disc {
-    fn to_internal(self) -> internal::Primitive {
-        internal::Disc {
-            center: self.center.to_internal(),
-            normal: self.normal.to_internal(),
-            radius: self.radius,
-            texture: self.texture.to_internal(),
-        }
-        .build()
-    }
-}
-
-#[pyclass]
-#[derive(Clone, Copy)]
-pub struct Cylinder {
-    #[pyo3(get, set)]
-    pub center1: Vec3,
-    #[pyo3(get, set)]
-    pub center2: Vec3,
-    #[pyo3(get, set)]
-    pub radius: f64,
-    #[pyo3(get, set)]
-    pub texture: Texture,
-}
-
-#[pymethods]
-impl Cylinder {
-    #[new]
-    pub fn new(center1: Vec3, center2: Vec3, radius: f64, texture: Texture) -> Self {
-        Self {
-            center1,
-            center2,
-            radius,
-            texture,
-        }
-    }
-}
-
-impl ToInternal for Cylinder {
-    fn to_internal(self) -> internal::Primitive {
-        internal::Cylinder {
-            center1: self.center1.to_internal(),
-            center2: self.center2.to_internal(),
-            radius: self.radius,
-            texture: self.texture.to_internal(),
-        }
-        .build()
-    }
-}
-
-#[pyclass]
-#[derive(Clone, Copy)]
-pub struct EmptyCone {
-    #[pyo3(get, set)]
-    pub orig: Vec3,
-    #[pyo3(get, set)]
-    pub dir: Vec3,
-    #[pyo3(get, set)]
-    pub angle: f64,
-    #[pyo3(get, set)]
-    pub begin: f64,
-    #[pyo3(get, set)]
-    pub end: f64,
-    #[pyo3(get, set)]
-    pub texture: Texture,
-}
-
-#[pymethods]
-impl EmptyCone {
-    #[new]
-    pub fn new(orig: Vec3, dir: Vec3, angle: f64, begin: f64, end: f64, texture: Texture) -> Self {
-        Self {
-            orig,
-            dir,
-            angle,
-            begin,
-            end,
-            texture,
-        }
-    }
-}
-
-impl ToInternal for EmptyCone {
-    fn to_internal(self) -> internal::Primitive {
-        internal::EmptyCone {
-            orig: self.orig.to_internal(),
-            dir: self.dir.to_internal(),
-            angle: self.angle,
-            begin: self.begin,
-            end: self.end,
-            texture: self.texture.to_internal(),
-        }.build()
-    }
-}
-
-#[pyclass]
-#[derive(Copy, Clone)]
-pub struct Cone {
-    #[pyo3(get, set)]
-    pub orig: Vec3,
-    #[pyo3(get, set)]
-    pub dir: Vec3,
-    #[pyo3(get, set)]
-    pub angle: f64,
-    #[pyo3(get, set)]
-    pub begin: f64,
-    #[pyo3(get, set)]
-    pub end: f64,
-    #[pyo3(get, set)]
-    pub texture: Texture,
-}
-
-#[pymethods]
-impl Cone {
-    #[new]
-    pub fn new(orig: Vec3, dir: Vec3, angle: f64, begin: f64, end: f64, texture: Texture) -> Self {
-        Self {
-            orig,
-            dir,
-            angle,
-            begin,
-            end,
-            texture,
-        }
-    }
-}
-
-impl ToInternal for Cone {
-    fn to_internal(self) -> internal::Primitive {
-        internal::Cone {
-            orig: self.orig.to_internal(),
-            dir: self.dir.to_internal(),
-            angle: self.angle,
-            begin: self.begin,
-            end: self.end,
-            texture: self.texture.to_internal(),
-        }.build()
-    }
+dvp!{Cone,
+    orig: Vec3,
+    dir: Vec3,
+    angle: f64,
+    begin: f64,
+    end: f64,
 }
