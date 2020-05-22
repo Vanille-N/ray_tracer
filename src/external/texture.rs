910422b2905a8f27a249bdefa2e7e1295a3d481e
Vanille-N <neven.villani@gmail.com>
2020-05-22 15:35:30 +0200

    incl const colors in lib by making them static fn. Also fewer lines.

--- src/external/texture.rs
+++ src/external/texture.rs
@@ -25,111 +25,44 @@ impl RGB {
     }
 }
 
-pub const RED: RGB = RGB {
-    r: 1.0,
-    g: 0.0,
-    b: 0.0,
-};
-pub const DKRED: RGB = RGB {
-    r: 0.5,
-    g: 0.0,
-    b: 0.0,
-};
-pub const LTRED: RGB = RGB {
-    r: 1.0,
-    g: 0.5,
-    b: 0.5,
-};
-pub const BLUE: RGB = RGB {
-    r: 0.0,
-    g: 0.0,
-    b: 1.0,
-};
-pub const DKBLUE: RGB = RGB {
-    r: 0.0,
-    g: 0.0,
-    b: 0.5,
-};
-pub const LTBLUE: RGB = RGB {
-    r: 0.3,
-    g: 0.6,
-    b: 1.0,
-};
-pub const CYAN: RGB = RGB {
-    r: 0.0,
-    g: 1.0,
-    b: 1.0,
-};
-pub const GREEN: RGB = RGB {
-    r: 0.0,
-    g: 1.0,
-    b: 0.0,
-};
-pub const DKGREEN: RGB = RGB {
-    r: 0.0,
-    g: 0.5,
-    b: 0.0,
-};
-pub const LTGREEN: RGB = RGB {
-    r: 0.7,
-    g: 1.0,
-    b: 0.0,
-};
-pub const PURPLE: RGB = RGB {
-    r: 0.7,
-    g: 0.0,
-    b: 0.0,
-};
-pub const MAGENTA: RGB = RGB {
-    r: 1.0,
-    g: 0.0,
-    b: 1.0,
-};
-pub const YELLOW: RGB = RGB {
-    r: 1.0,
-    g: 1.0,
-    b: 0.0,
-};
-pub const BROWN: RGB = RGB {
-    r: 0.3,
-    g: 0.2,
-    b: 0.0,
-};
-pub const ORANGE: RGB = RGB {
-    r: 1.0,
-    g: 0.4,
-    b: 0.0,
-};
-pub const TURQUOISE: RGB = RGB {
-    r: 0.0,
-    g: 0.9,
-    b: 0.6,
-};
-pub const BLACK: RGB = RGB {
-    r: 0.0,
-    g: 0.0,
-    b: 0.0,
-};
-pub const WHITE: RGB = RGB {
-    r: 1.0,
-    g: 1.0,
-    b: 1.0,
-};
-pub const GREY: RGB = RGB {
-    r: 0.5,
-    g: 0.5,
-    b: 0.5,
-};
-pub const DKGREY: RGB = RGB {
-    r: 0.2,
-    g: 0.2,
-    b: 0.2,
-};
-pub const LTGREY: RGB = RGB {
-    r: 0.8,
-    g: 0.8,
-    b: 0.8,
-};
+macro_rules! color {
+    ( $name:ident : $alias:tt, $r:expr, $g:expr, $b:expr ) => {
+        #[pymethods]
+        impl RGB {
+            #[staticmethod]
+            #[text_signature = "(/)"]
+            pub fn $name () -> RGB {
+                RGB {
+                    r: $r,
+                    g: $g,
+                    b: $b,
+                }
+            }
+        }
+    }
+}
+
+color!(red: _, 1.0, 0.0, 0.0);
+color!(dkred: drd, 0.5, 0.0, 0.0);
+color!(ltred: lrd, 1.0, 0.5, 0.5);
+color!(blue: blu, 0.0, 0.0, 1.0);
+color!(dkblue: dbl, 0.0, 0.0, 0.5);
+color!(ltblue: lbl, 0.3, 0.6, 1.0);
+color!(cyan: cyn, 0.0, 1.0, 1.0);
+color!(green: grn, 0.0, 1.0, 0.0);
+color!(dkgreen: dgr, 0.0, 0.5, 0.0);
+color!(ltgreen: lgr, 0.7, 1.0, 0.0);
+color!(purple: ppl, 0.7, 0.0, 0.0);
+color!(magenta: mgt, 1.0, 0.0, 1.0);
+color!(yellow: ylw, 1.0, 1.0, 0.0);
+color!(brown: brn, 0.3, 0.2, 0.0);
+color!(orange: org, 1.0, 0.4, 0.0);
+color!(turquoise: tqs, 0.0, 0.9, 0.6);
+color!(black: blk, 0.0, 0.0, 0.0);
+color!(white: wht, 1.0, 1.0, 1.0);
+color!(grey: gry, 0.5, 0.5, 0.5);
+color!(dkgrey: dgy, 0.2, 0.2, 0.2);
+color!(ltgrey: lgy, 0.8, 0.8, 0.8);
 
 #[pyclass]
 #[derive(Clone, Copy)]
