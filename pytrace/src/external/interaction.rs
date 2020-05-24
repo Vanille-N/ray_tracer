c7b340d03447913f80b9584d499fca338d08ed28
Vanille-N <neven.villani@gmail.com>
2020-05-24 08:39:56 +0200

    New crate: pytrace Interface with Python via PyO3. Work in progress

---
+++ pytrace/src/external/interaction.rs
@@ -0,0 +1,52 @@
+use crate::internal;
+use pyo3::prelude::*;
+use std::sync::Arc;
+
+pub trait ToInternal {
+    fn to_internal(&self) -> internal::Primitive;
+}
+
+#[pyclass]
+#[derive(Clone)]
+pub struct Primitive {
+    pub obj: Arc<dyn ToInternal>,
+}
+
+impl Primitive {
+    pub fn extract(self) -> internal::Interaction {
+        self.obj.to_internal().wrap()
+    }
+
+    pub fn wrap(self) -> InterTree {
+        InterTree::Item(self)
+    }
+}
+
+pub enum Interaction {
+    Inter,
+    Diff,
+    Union,
+}
+
+pub enum InterTree {
+    Item(Primitive),
+    Node(Interaction, Box<InterTree>, Box<InterTree>),
+}
+
+impl InterTree {
+    pub fn inter(self, other: Self) -> Self {
+        Self::Node(Interaction::Inter, Box::new(self), Box::new(other))
+    }
+
+    pub fn diff(self, other: Self) -> Self {
+        Self::Node(Interaction::Diff, Box::new(self), Box::new(other))
+    }
+
+    pub fn union(self, other: Self) -> Self {
+        Self::Node(Interaction::Union, Box::new(self), Box::new(other))
+    }
+
+    pub fn canonical(self) -> Vec<internal::Interaction> {
+        unimplemented!()
+    }
+}
---
+++ pytrace/src/external/primitives.rs
@@ -0,0 +1,113 @@
+use crate::external::*;
+use libtrace::internal;
+use pyo3::prelude::*;
+use std::sync::Arc;
+
+macro_rules! internalize {
+    ( $caller:ident, $member:ident, f64 ) => {
+        $caller.$member
+    };
+    ( $caller:ident, $member:ident, $t:tt ) => {
+        $caller.$member.to_internal()
+    };
+}
+
+macro_rules! dvp {
+    ($name:ident, $( $member:ident : $t:tt, )*) => {
+        #[pyclass]
+        #[derive(Copy, Clone)]
+        pub struct $name {
+            $(
+                #[pyo3(get, set)] pub $member: $t,
+            )*
+            #[pyo3(get, set)] pub texture: Texture,
+        }
+
+
+        #[pymethods]
+        impl $name {
+            #[new]
+            #[allow(clippy::new_ret_no_self)]
+            pub fn new( $( $member: $t, )* texture: Texture ) -> Primitive {
+                Primitive {
+                    obj: Arc::new(Self {
+                        $( $member, )*
+                        texture,
+                    }),
+                }
+            }
+        }
+
+        impl ToInternal for $name {
+            fn to_internal(&self) -> internal::Primitive {
+                internal:: $name {
+                    $( $member: internalize![self, $member, $t], )*
+                    texture: self.texture.to_internal(),
+                }.build()
+            }
+        }
+    }
+}
+
+dvp! {Sphere,
+    center: Vec3,
+    radius: f64,
+}
+
+dvp! {InfinitePlane,
+    orig: Vec3,
+    normal: Vec3,
+}
+
+dvp! {Triangle,
+    a: Vec3,
+    u: Vec3,
+    v: Vec3,
+}
+
+dvp! {Parallelogram,
+    a: Vec3,
+    u: Vec3,
+    v: Vec3,
+}
+
+dvp! {Rhomboid,
+    a: Vec3,
+    u: Vec3,
+    v: Vec3,
+    w: Vec3,
+}
+
+dvp! {EmptyCylinder,
+    center1: Vec3,
+    center2: Vec3,
+    radius: f64,
+}
+
+dvp! {Disc,
+    center: Vec3,
+    normal: Vec3,
+    radius: f64,
+}
+
+dvp! {Cylinder,
+    center1: Vec3,
+    center2: Vec3,
+    radius: f64,
+}
+
+dvp! {EmptyCone,
+    orig: Vec3,
+    dir: Vec3,
+    angle: f64,
+    begin: f64,
+    end: f64,
+}
+
+dvp! {Cone,
+    orig: Vec3,
+    dir: Vec3,
+    angle: f64,
+    begin: f64,
+    end: f64,
+}
