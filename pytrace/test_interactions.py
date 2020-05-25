5943f7e858d14061f22665cd220a08c806d25c5a
Vanille-N <neven.villani@gmail.com>
2020-05-25 14:58:17 +0200

    Python script to test new options (bug detected)

---
+++ pytrace/test_interactions.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python3
+
+from pytrace import *
+
+tr = Cfg(100, 100, 20)
+tr.set_sky(Sky("../data/sky.ppm"))
+cam = Camera(0, 0, 0)
+cam.distance = 8
+tr.set_cam(cam)
+
+a = Sphere(Vec3(0, 0, 0), 1, Texture.lambertian(RGB(1, 0, 0)))
+b = Sphere(Vec3(1, 0, 0), 1, Texture.lambertian(RGB(0, 1, 0)))
+c = Sphere(Vec3(0.5, 0.5, 0), 1, Texture.lambertian(RGB(0, 0, 1)))
+
+tr.add_obj(a.diff(b).diff(c))
+
+tr.render("union")
