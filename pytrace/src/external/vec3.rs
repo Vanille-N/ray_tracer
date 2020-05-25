34cea9c709fe71854284e108254b2cb6d54df6f9
Vanille-N <neven.villani@gmail.com>
2020-05-25 16:30:00 +0200

    for now, Python can use .mul and .add, I hope to soon implement + and *

--- pytrace/src/external/vec3.rs
+++ pytrace/src/external/vec3.rs
@@ -19,6 +19,14 @@ impl Vec3 {
     pub fn new(x: f64, y: f64, z: f64) -> Self {
         Self { x, y, z }
     }
+
+    pub fn mul(self, f: f64) -> Self {
+        Self { x: self.x * f, y: self.y * f, z: self.z * f }
+    }
+
+    pub fn add(self, v: Self) -> Self {
+        Self { x: self.x + v.x, y: self.y + v.y, z: self.z + v.z }
+    }
 }
 
 impl Vec3 {
@@ -26,3 +34,12 @@ impl Vec3 {
         internal::Vec3(self.x, self.y, self.z)
     }
 }
+
+// Fails for some unknown reason...
+
+// #[pyproto]
+// impl PyNumberProtocol for Vec3 {
+//     fn __add__(&self, other: &Vec3) -> PyResult<Vec3> {
+//         Ok(Vec3{x: self.x + other.x, y: self.y + other.y, z: self.z + other.z})
+//     }
+// }
