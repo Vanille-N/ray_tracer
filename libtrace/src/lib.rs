c447edbc8dfa996dd4c3360136d7ba86c57ddddc
Vanille-N <neven.villani@gmail.com>
2020-05-31 14:46:01 +0200

    module doc

--- libtrace/src/lib.rs
+++ libtrace/src/lib.rs
@@ -1,3 +1,6 @@
+/// A collection of complex objects
 pub mod composite;
+/// All of the objects and functions required for the proper execution of the physics engine
 pub mod internal;
+/// Main function
 pub mod render;
