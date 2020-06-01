ce54789ff1119c186dc2571b186f6274b5b78d28
Vanille-N <neven.villani@gmail.com>
2020-06-01 14:24:32 +0200

    doc for internal mod

--- libtrace/src/internal/mod.rs
+++ libtrace/src/internal/mod.rs
@@ -1,10 +1,18 @@
+/// Abstaction for the field of view
 pub mod camera;
+/// Wrappers, trait & textures
 pub mod hitable;
+/// Basic objects to build complex scenes
 pub mod primitives;
+/// Simple representation of a light ray
 pub mod ray;
+/// Operations on color values
 pub mod rgb;
+/// External environment texture
 pub mod sky;
+/// Used for both positions and 3D-vectors
 pub mod vec3;
+/// Main loop & wrapper struct
 pub mod world;
 
 pub use camera::Camera;
@@ -16,4 +24,5 @@ pub use sky::Sky;
 pub use vec3::Vec3;
 pub use world::World;
 
+/// To prevent "shadow acne"
 pub const EPSILON: f64 = 0.000_000_1;
