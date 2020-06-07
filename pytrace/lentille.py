46206f4534f82cf0227619f858f378d3e110dc22
Vanille-N <neven.villani@gmail.com>
2020-06-07 16:51:20 +0200

    update script

--- pytrace/lentille.py
+++ pytrace/lentille.py
@@ -19,16 +19,17 @@ s2 = Sphere(Vec3(0, 0, -e/2 + r), r, glass)
 
 rtext = Texture.lambertian(RGB(0.1, 0.1, 0.1))
 
-ring = Cylinder(Vec3(0, 0, e/10), Vec3(0, 0, -e/10), (e*r)**(0.5), rtext).diff(Cylinder(Vec3(0, 0, e), Vec3(0, 0, -e), (e*r)**(0.5) * 0.9, rtext))
+ring = Cylinder(Vec3(0, 0, e/10), Vec3(0, 0, -e/10), (e*r)**(0.5), rtext) \
+    - Cylinder(Vec3(0, 0, e), Vec3(0, 0, -e), (e*r)**(0.5) * 0.9, rtext)
 
 def target(y, z):
     orig = Vec3(-2, y, z)
     bg = Texture.lambertian(RGB(0.5, 0.8, 0))
     fg = Texture.lambertian(RGB(0, 0.2, 1))
     triangle = Triangle(orig, Vec3(60, 0, 0), Vec3(0, 20, 0), bg)
-    arrowrec = Parallelogram(orig.add(Vec3(2, 1, 0.01)), Vec3(1, 0, 0), Vec3(0, 3, 0), fg)
-    arrowhead = Triangle(orig.add(Vec3(2.5, 6, 0.01)), Vec3(-1, -2, 0), Vec3(1, -2, 0), fg)
-    return triangle.union(arrowrec).union(arrowhead)
+    arrowrec = Parallelogram(orig + Vec3(2, 1, 0.01), Vec3(1, 0, 0), Vec3(0, 3, 0), fg)
+    arrowhead = Triangle(orig + Vec3(2.5, 6, 0.01), Vec3(-1, -2, 0), Vec3(1, -2, 0), fg)
+    return triangle | arrowrec | arrowhead
 
 cam.distance = 100
 tr.set_cam(cam)
@@ -41,7 +42,7 @@ def lpad(l, n):
 tr.start_movie("lens")
 
 
-tr.add_obj(s1.inter(s2))
+tr.add_obj(s1 & s2)
 tr.add_obj(target(15, -10))
 tr.add_obj(ring)
 for i in range(50):
@@ -59,7 +60,7 @@ tr.set_cam(cam)
 
 for i in range(60, -9, -1):
     tr.clear()
-    tr.add_obj(s1.inter(s2))
+    tr.add_obj(s1 & s2)
     tr.add_obj(target(i/4, -10))
     tr.add_obj(ring)
     tr.frame()
@@ -69,7 +70,7 @@ for i in range(20):
 
 for i in range(120):
     tr.clear()
-    tr.add_obj(s1.inter(s2))
+    tr.add_obj(s1 & s2)
     tr.add_obj(target(-2, -10-i))
     tr.add_obj(ring)
     tr.frame()
